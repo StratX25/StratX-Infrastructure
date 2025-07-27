9d9bcd6de4372500cec87f1c4fbc2d3e25ec637a
stratX <stratx25@gmail.com>
2025-07-27 16:52:13 -0400

    Update metrics_pull.py

--- github/metrics_pull.py
+++ github/metrics_pull.py
@@ -3,6 +3,11 @@ import csv
 import os
 from datetime import datetime
 
+# Load secrets securely
+bot_token = os.environ.get("BOT_TOKEN")
+chat_id = os.environ.get("CHAT_ID")
+token = os.environ.get("GITHUB_PAT") or os.environ.get("GITHUB_TOKEN")
+
 # Telegram alert logic (FREE)
 
 bot_token = os.environ.get("8461268130:AAF8FSAegC45r8_GhnUJ-3CKilCJHFozyVo")
